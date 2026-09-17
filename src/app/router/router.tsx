8ecb10f22c31a16c0d088f70b8cf379df84b49a1
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2026-09-17 12:50:47 +0200

    refactor: add base name

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -4,46 +4,51 @@ import Login from '@/pages/login/Login.tsx';
 import { Spinner } from '@/shared/ui/spinner/Spinner.tsx';
 import NotFound from '@/pages/not-found/NotFound.tsx';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      HydrateFallback: Spinner,
+      children: [
+        {
+          path: '/login',
+          element: <Login />,
+        },
+        {
+          element: <ProtectedRoute />,
+          HydrateFallback: Spinner,
+          children: [
+            {
+              path: '/',
+              element: <Navigate to="/dashboard" replace />,
+            },
+            {
+              path: '/dashboard',
+              lazy: async () => {
+                const module = await import('@/pages/dashboard/DashboardPage');
+                return { Component: module.default };
+              },
+            },
+            {
+              path: '/products',
+              lazy: async () => {
+                const module = await import('@/pages/medications/MedicationList.tsx');
+                return { Component: module.default };
+              },
+            },
+            {
+              path: '/products/:id',
+              lazy: async () => {
+                const module = await import('@/pages/medication/MedicineCard');
+                return { Component: module.default };
+              },
+            },
+            { path: '*', element: <NotFound /> },
+          ],
+        },
+      ],
+    },
+  ],
   {
-    HydrateFallback: Spinner,
-    children: [
-      {
-        path: '/login',
-        element: <Login />,
-      },
-      {
-        element: <ProtectedRoute />,
-        HydrateFallback: Spinner,
-        children: [
-          {
-            path: '/',
-            element: <Navigate to="/dashboard" replace />,
-          },
-          {
-            path: '/dashboard',
-            lazy: async () => {
-              const module = await import('@/pages/dashboard/DashboardPage');
-              return { Component: module.default };
-            },
-          },
-          {
-            path: '/products',
-            lazy: async () => {
-              const module = await import('@/pages/medications/MedicationList.tsx');
-              return { Component: module.default };
-            },
-          },
-          {
-            path: '/products/:id',
-            lazy: async () => {
-              const module = await import('@/pages/medication/MedicineCard');
-              return { Component: module.default };
-            },
-          },
-          { path: "*", element: <NotFound /> },
-        ],
-      },
-    ],
+    basename: import.meta.env.VITE_BASE_URL,
   },
-]);
+);
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/medicineJournal/',
   plugins: [react(),svgr({include: '**/*.svg',})],
   resolve: {
     alias: {
